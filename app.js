a3e34430dab2955cc24d70cb405a1b2b9dfcee64
juanfperez <juanfperez@me.com>
2016-07-25 20:40:55 -0400

    mvc

--- app.js
+++ app.js
@@ -14,17 +14,26 @@
 
   this.isSet = function(tabName){
     return this.tab === tabName;
-  }; 
+    };
+  });
+
+  app.controller('SkillsController', function () {
+    this.current = 0;
+
+    this.setCurrent = function (current) {
+      this.current = current || 0;
+    };
   });
 
   var gems = [
     {
       name: "Juan Perez",
       image: "profile.png",
-      intro: "bajkfldsajfkdl;sa",
+      intro: "Here is a basic Angular Application that I think meets some of the criteria you are looking for. This is something I whipped up on a fly after reading documentation for less than a couple of hours. There is still a lot of work to be done, but this gives you an idea of how much I can learn and how fast!",
       email: "juanfperez@me.com",
       phone: "954-394-6357",
       skills: [
+        "Overall Code Killer!",
         "Full-Stack Web Development",
         "Responsive Web Design",
         "Ruby",
